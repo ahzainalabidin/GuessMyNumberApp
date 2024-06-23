4b090c60a1e8266ccab4adf2ac068f84c1cae80a
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-23 13:29:41 +0800

    Create ripple effect on Android

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -6,23 +6,30 @@ function pressHandler() {
 
 const PrimaryButton = ({ children }) => {
     return (
-        <Pressable onPress={pressHandler}>
-            <View style={styles.buttonContainer}>
+        <View style={styles.buttonOuterContainer}>
+            <Pressable
+                style={styles.buttonInnerContainer}
+                onPress={pressHandler}
+                android_ripple={{ color: '#640233' }
+                }>
                 <Text style={styles.buttonText}>{children}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
     );
 }
 
 export default PrimaryButton;
 
 const styles = StyleSheet.create({
-    buttonContainer: {
-        backgroundColor: '#72063c',
+    buttonOuterContainer: {
         borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#72063c',
         paddingVertical: 8,
         paddingHorizontal: 16,
-        margin: 4,
         elevation: 2
     },
     buttonText: {
