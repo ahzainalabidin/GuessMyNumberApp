b7b6d8edfba35b10c487ffe0a880de6e5c341154
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-23 14:56:09 +0800

    Add background colour for main screen

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  rootScreen: {
+    flex: 1,
+    backgroundColor: '#ddb52f'
+  }
 });
