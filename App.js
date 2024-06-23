935983c1ca57e6d57ca1c2308bf9e95846b23f17
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-23 15:21:12 +0800

    Change background colour to linear gradient colour

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient style={styles.rootScreen} colors={['#4e0329', '#ddb52f']}>
       <StartGameScreen />
-    </View>
+    </LinearGradient >
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
-    flex: 1,
-    backgroundColor: '#ddb52f'
+    flex: 1
   }
 });
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -37,7 +37,7 @@ const styles = StyleSheet.create({
         marginTop: 100,
         marginHorizontal: 24,
         padding: 16,
-        backgroundColor: '#4e0329',
+        backgroundColor: '#3b021f',
         borderRadius: 8,
         elevation: 4,
         shadowColor: 'black',
