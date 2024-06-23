133d2ee8ebbc8d83943c1c099cd915198a65beb6
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-23 21:04:10 +0800

    Style screen's title

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -4,7 +4,7 @@ const GameScreen = () => {
     return (
         <View style={styles.screen}>
 
-            <Text>Opponent's Guess</Text>
+            <Text style={styles.title}>Opponent's Guess</Text>
 
             <Text>Guess</Text>
 
@@ -27,5 +27,14 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 24
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#ddb52f',
+        textAlign: 'center',
+        borderWidth: 2,
+        borderColor: '#ddb52f',
+        padding: 12
     }
 });
