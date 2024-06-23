f0690b37245af320b165139163da191ed3127cd3
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-23 13:16:52 +0800

    Add styles to button

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,15 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+function pressHandler() {
+    console.log('Pressed!');
+};
 
 const PrimaryButton = ({ children }) => {
     return (
-        <View>
-            <Text>{children}</Text>
-        </View>
+        <Pressable onPress={pressHandler}>
+            <View style={styles.container}>
+                <Text>{children}</Text>
+            </View>
+        </Pressable>
     );
 }
 
 export default PrimaryButton;
 
 const styles = StyleSheet.create({
-
+    container: {
+        backgroundColor: '#72063c',
+        borderRadius: 28,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        margin: 4,
+        elevation: 2
+    }
 });
