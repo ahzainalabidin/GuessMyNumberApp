27d1b79cba30bfbc31f4fe86f1e360866b481e5c
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-23 08:01:37 +0800

    Add styles for input container

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,7 +3,7 @@ import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
     return (
-        <View>
+        <View style={styles.inputContainer}>
             <TextInput />
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
@@ -14,5 +14,11 @@ const StartGameScreen = () => {
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-
+    inputContainer: {
+        marginTop: 100,
+        marginHorizontal: 24,
+        padding: 16,
+        backgroundColor: '#72063c',
+        borderRadius: 8
+    }
 });
