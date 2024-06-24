71ac41ff6522c290e159dc0319019c369fecc972
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-24 10:09:30 +0800

    Add component for number container

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,13 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native';
-import Title from '../components/Title';
+import { useState } from 'react';
 
-const GameScreen = () => {
+import Title from '../components/ui/Title';
+import NumberContainer from '../components/game/NumberContainer';
+
+function generateRandomBetween(min, max, exclude) {
+
+    const randomNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (randomNum === exclude) {
+        return generateRandomBetween(min, max, exclude);
+    } else {
+        return randomNum;
+    }
+
+}
+
+const GameScreen = ({ userNumber }) => {
+
+    const initialGuess = generateRandomBetween(1, 100, userNumber);
+    const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
     return (
         <View style={styles.screen}>
 
             <Title>Opponent's Guess</Title>
 
-            <Text>Guess</Text>
+            <NumberContainer>{currentGuess}</NumberContainer>
 
             <View>
                 <Text>Higher or Lower?</Text>
