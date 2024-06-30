c569cad12033e52254d52533a73e212ec5bb11d6
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-30 23:31:44 +0800

    Create game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,9 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import Title from '../components/ui/Title';
+import Colors from '../constants/colors';
 
 const GameOverScreen = () => {
     return (
-        <View style={styles.screen}>
-            <Text>Game is over!</Text>
+        <View style={styles.rootContainer}>
+
+            <Title>GAME OVER!</Title>
+
+            <View style={styles.imageContainer}>
+                <Image
+                    source={require('../assets/images/success.png')}
+                    style={styles.image}
+                />
+            </View>
+
+            <Text>Your phone needed X rounds to guess the number Y.</Text>
+
         </View>
     );
 }
@@ -11,8 +24,23 @@ const GameOverScreen = () => {
 export default GameOverScreen;
 
 const styles = StyleSheet.create({
-    screen: {
+    rootContainer: {
         flex: 1,
-        padding: 24
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden',
+        margin: 36
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 });
