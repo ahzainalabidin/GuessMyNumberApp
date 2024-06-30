3c51723f60f9ed1e94ff091734913273f40aaabb
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2024-06-30 22:50:36 +0800

    Use custom font as font family

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -22,8 +22,8 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     numberText: {
+        fontFamily: 'open-sans-bold',
         color: Colors.accent500,
-        fontSize: 36,
-        fontWeight: 'bold'
+        fontSize: 36
     }
 });
--- components/ui/InstructionText.js
+++ components/ui/InstructionText.js
@@ -13,6 +13,7 @@ export default InstructionText;
 
 const styles = StyleSheet.create({
     instructionText: {
+        fontFamily: 'open-sans',
         color: Colors.accent500,
         fontSize: 24
     }
